35671471b1a02b0c8c3b1055e99bb1c18c8c4cc6
Jerzy Brzozowski <jerzy-brzozowski@wp.pl>
2019-05-22 18:20:08 +0200

    Move from SCSS to PostCSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,12 @@
 module.exports = {
 	plugins: [
+		require('postcss-import'),
+		require('postcss-simple-vars'),
+		require('postcss-extend'),
+		require('postcss-nested'),
 		require('cssnano')({
 			preset: 'default',
 		}),
-		require('postcss-nested'),
+
 	],
 };
